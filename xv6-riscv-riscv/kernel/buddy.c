c483b50fdaac5d36c9051312ccb1e10685cfe639
Jovan <jovan.zivanovic07@gmail.com>
2026-02-05 15:22:22 +0100

    Initial commit - project base

---
+++ xv6-riscv-riscv/kernel/buddy.c
@@ -0,0 +1,5 @@
+//
+// Created by os on 2/5/26.
+//
+
+#include "buddy.h"
---
+++ xv6-riscv-riscv/kernel/buddy.h
@@ -0,0 +1,8 @@
+//
+// Created by os on 2/5/26.
+//
+
+#ifndef XV6_RISCV_RISCV_V1_0_BUDDY_H
+#define XV6_RISCV_RISCV_V1_0_BUDDY_H
+
+#endif //XV6_RISCV_RISCV_V1_0_BUDDY_H
