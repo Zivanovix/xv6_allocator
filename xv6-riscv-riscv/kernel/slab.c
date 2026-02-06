fed554b8048c18918e838d825a3045de38662543
Jovan <jovan.zivanovic07@gmail.com>
2026-02-06 13:06:54 +0100

    added cache kreate

--- xv6-riscv-riscv/kernel/slab.c
+++ xv6-riscv-riscv/kernel/slab.c
@@ -2,10 +2,10 @@
 #include "spinlock.h"
 #include "riscv.h"
 #include "defs.h"
-
+#include "slab.h"
 
 // Align obj size to a first greater number that is divisible by 8
-#define ALIGIN(size) (((size) + 7) & ~7)
+#define ALIGN(size) (((size) + 7) & ~7)
 
 // metadata about a single slab
 typedef struct slab_s {
@@ -33,21 +33,69 @@ struct kmem_cache_s {
 
     kmem_cache_t* next_cache;
     int error_code;
-    int in_use; // flag for static pool of cahces
+    int in_use; // flag for static pool of caches
 };
 
 #define MAX_CACHES 40
 static kmem_cache_t cache_pool[MAX_CACHES];
 static kmem_cache_t* cache_chain = 0; // head of all caches list
-
+struct spinlock cache_chain_lock; // Global lock for chace list
 
 void kmem_init(void* space, int block_num) {
     buddy_init(space, block_num);
 
-    initlock(&cache_chain.lock, "kmem_cache_chain");
+    initlock(&cache_chain_lock, "kmem_cache_chain");
 
     for(int i = 0; i < MAX_CACHES; ++i) {
         cache_pool[i].in_use = 0;
         initlock(&cache_pool[i].lock, "cache_lock");
     }
 }
+
+kmem_cache_t* kmem_cache_create(const char* name, size_t size, void (*ctor)(void*), void (*dtor)(void*)) {
+    kmem_cache_t* cp = 0;
+    size_t aligned_size = ALIGN(size);
+
+    acquire(&cache_chain_lock);
+    for(int i = 0; i < MAX_CACHES; ++i) {
+        if(cache_pool[i].in_use == 0) {
+            cp = cache_pool + i;
+            cp->in_use = 1;
+            break;
+        }
+    }
+
+    release(&cache_chain_lock);
+    if(cp == 0) return 0; // Error! maximum number of caches in system reached
+
+    safestrcpy(cp->name, name, sizeof(cp->name));
+    cp->obj_size = aligned_size;
+    cp->ctor = ctor;
+    cp->dtor = dtor;
+    cp->error_code = 0;
+    cp->slabs_full = 0;
+    cp->slabs_partial = 0;
+    cp->slabs_free = 0;
+
+    size_t slab_header_size = ALIGN(sizeof(slab_t));
+    size_t min_need = slab_header_size + aligned_size;
+    int pages = (min_need + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    // size of the slab must be the power of two because of buddy
+    int actual_pages = 1;
+    while(actual_pages < pages) {
+        actual_pages <<= 1;
+    }
+    cp->slab_pages = actual_pages;
+
+    size_t total_mem = (size_t)actual_pages * BLOCK_SIZE;
+    cp->obj_per_slab = (total_mem - slab_header_size) / aligned_size;
+
+    acquire(&cache_chain_lock);
+    cp->next_cache = cache_chain;
+    cache_chain = cp;
+    release(&cache_chain_lock);
+
+    return cp;
+}
+
